7462c265123541448530cfee589c1b17cde9e80f
OM Miles <olivia.m.miles@gmail.com>
2021-04-11 11:32:49 -0700

    Notes added

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+// function expression called divide
 const divide = () => 2000 / 100
 
+// arrow function called square
+//square takes one param and multiplies it times itself
 const square = (param) => param *= param
 
+// arrow function called add
+// arrow takes two parameters and adds them together
 const add = (param1, param2) => param1 + param2
